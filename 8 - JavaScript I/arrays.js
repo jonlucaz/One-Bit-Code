f9e0de5b8a0dcf08878aaa4fddf6df01f1dafa79
jonlucaz <jonathanlucastri@gmail.com>
2022-11-19 10:50:09 -0300

    novas aulas de estrutura de dados e arrays

---
+++ 8 - JavaScript I/arrays.js
@@ -0,0 +1,22 @@
+/*
+Arrays (Vetores)
+  - É uma estrutura no js para trabalhar com todo tipo de lista;
+  - Seus elementos são indicados por um indice inteiro e começa no 0;
+  - Pode ser criado através de colchetes [];
+  - Pode ser de qualquer tipo e tamanho 
+*/
+
+const listaDeCompras = [];
+console.log(listaDeCompras);
+
+listaDeCompras[0] = "Arroz";
+
+console.log(listaDeCompras);
+
+listaDeCompras[1] = "Feijão";
+
+console.log(listaDeCompras);
+
+listaDeCompras.push = "Batata";
+
+console.log(listaDeCompras);
---
+++ 8 - JavaScript I/estruturaDeDados.js
@@ -0,0 +1,19 @@
+//Estrutura de Dados
+/*
+São um modo particular de armazenar e organizar dados, para que sejam usados posteriormente.
+
+Estrutura de dados clássica:
+    - Lista Ligada: Coleção ordenada de valores onde cada elemento aponta para o próximo;
+    - Array: Coleção de elementos que podem ser identificados por um índice;
+    - Fila (queue): Coleção de elementos onde o primeiro elemento a ser adicionado é o primeiro a ser retirado, FIFO (First In, First Out);
+    - Pilha (stack): Primeiro elemento a ser adicionado é o último a ser retirado, FILO (First In, Last Out);
+    - Arvore (tree): Conjunto de elementos chamados de nós(nodes) organizados em estrutura hierárquica (não sequencial), podendo estar
+    "abaixo" ou "acima" de outros nós.
+    Uma arvore genealogica é um bom exemplo;
+
+Estrutura de dados no JavaScript (e em outras linguagens de alto nível):
+    - Lista (Array)
+    - Dicionários (Objeto) 
+
+
+*/
